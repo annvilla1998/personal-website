28b9dede2bc02de58586920cf9ad78ca3eacc078
annvilla1998 <villalobos1998@gmail.com>
2024-12-11 14:57:13 -0800

    Add minimized navigation menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-const navItems = document.querySelectorAll(".nav-item");
+document.addEventListener("DOMContentLoaded", function () {
+    const navItems = document.querySelectorAll(".nav-item");
+    const menuButton = document.querySelector("i.fa-solid.fa-bars");
+    const mobileMenu = document.querySelector("nav ul.minimized-menu");
 
-navItems.forEach(item => {
+  navItems.forEach((item) => {
     item.addEventListener("click", function () {
-        navItems.forEach(a => {
-            a.classList.remove("active");
-        })
+      navItems.forEach((a) => {
+        a.classList.remove("active");
+      });
 
-        item.classList.add("active");
-    })
-})
+      item.classList.add("active");
+      mobileMenu.classList.remove("active");
+    });
+  });
+
+
+  menuButton.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active");
+  });
+
+});
