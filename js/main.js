439cb58dea83efb149d194e4ff096351dfe6ddf1
annvilla1998 <villalobos1998@gmail.com>
2024-12-17 17:13:57 -0800

    Add image change

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
+const imageSources = {
+  camplife: [
+    "./assets/images/projects/camplife/home.png",
+    "./assets/images/projects/camplife/login.png",
+    "./assets/images/projects/camplife/book.png",
+    "./assets/images/projects/camplife/reviews.png",
+  ],
+  "crown-clothing": [
+    "./assets/images/projects/crown-clothing/home.png",
+    "./assets/images/projects/crown-clothing/cart.png",
+    "./assets/images/projects/crown-clothing/checkout.png",
+    "./assets/images/projects/crown-clothing/checkout-2.png",
+  ],
+  wedding: [
+    "./assets/images/projects/wedding-website/home.png",
+    "./assets/images/projects/wedding-website/rsvp.png",
+    "./assets/images/projects/wedding-website/photo-album.png",
+  ],
+};
+const currentIndexes = {
+  camplife: 0,
+  "crown-clothing": 0,
+  wedding: 0,
+};
+
 document.addEventListener("DOMContentLoaded", function () {
-    const navItems = document.querySelectorAll(".nav-item");
-    const menuButton = document.querySelector("i.fa-solid.fa-bars");
-    const mobileMenu = document.querySelector("nav ul.minimized-menu");
+  const navItems = document.querySelectorAll(".nav-item");
+  const menuButton = document.querySelector("i.fa-solid.fa-bars");
+  const mobileMenu = document.querySelector("nav ul.minimized-menu");
 
   navItems.forEach((item) => {
     item.addEventListener("click", function () {
@@ -14,9 +39,44 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-
   menuButton.addEventListener("click", function () {
     mobileMenu.classList.toggle("active");
   });
 
+  document.querySelectorAll(".project").forEach((projectElement) => {
+    const projectClass =
+      projectElement.querySelector(".project-container").classList[1];
+
+    projectElement
+      .querySelector(".fa-chevron-left")
+      .addEventListener("click", () => {
+        updateImage(projectClass, "left");
+      });
+
+    projectElement
+      .querySelector(".fa-chevron-right")
+      .addEventListener("click", () => {
+        updateImage(projectClass, "right");
+      });
+  });
 });
+
+function updateImage(projectClass, direction) {
+  const projectImages = imageSources[projectClass];
+  let currentIndex = currentIndexes[projectClass];
+
+  if (direction === "left") {
+    currentIndex =
+      (currentIndex - 1 + projectImages.length) % projectImages.length;
+  } else if (direction === "right") {
+    currentIndex = (currentIndex + 1) % projectImages.length;
+  }
+
+  const projectContainer = document.querySelector(
+    `.project-container.${projectClass}`
+  );
+  const imgElement = projectContainer.querySelector("img");
+  imgElement.src = projectImages[currentIndex];
+
+  currentIndexes[projectClass] = currentIndex;
+}
