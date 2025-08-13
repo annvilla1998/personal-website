a51403b66989a4edf79a4d1a2ca8f8ce9ef2ae23
annvilla1998 <villalobos1998@gmail.com>
2025-08-13 07:34:25 -0700

    Fix project arrow issue

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 const imageSources = {
-  camplife: [
-    "./assets/images/projects/camplife/home.png",
-    "./assets/images/projects/camplife/login.png",
-    "./assets/images/projects/camplife/book.png",
-    "./assets/images/projects/camplife/reviews.png",
+  "crown-clothing": [
+    "./assets/images/projects/crown-clothing/home.png",
+    "./assets/images/projects/crown-clothing/login.png",
+    "./assets/images/projects/crown-clothing/cart.png",
+    "./assets/images/projects/crown-clothing/checkout.png",
+    "./assets/images/projects/crown-clothing/checkout-2.png",
   ],
-  theturn: [
-    "./assets/images/projects/theturn/theturn.png",
-  ]
+  theturn: ["./assets/images/projects/theturn/theturn.png"],
 };
 const currentIndexes = {
   camplife: 0,
@@ -39,23 +38,29 @@ document.addEventListener("DOMContentLoaded", function () {
     const projectClass =
       projectElement.querySelector(".project-container").classList[1];
 
-    projectElement
-      .querySelector(".fa-chevron-left")
-      .addEventListener("click", () => {
-        updateImage(projectClass, "left");
-      });
+    const leftArrow = projectElement.querySelector(".fa-chevron-left");
+    const rightArrow = projectElement.querySelector(".fa-chevron-right");
 
-    projectElement
-      .querySelector(".fa-chevron-right")
-      .addEventListener("click", () => {
-        updateImage(projectClass, "right");
-      });
+    if (!rightArrow || !leftArrow) {
+      return;
+    }
+
+    rightArrow.addEventListener("click", () => {
+      updateImage(projectClass, "right");
+    });
+    leftArrow.addEventListener("click", () => {
+      updateImage(projectClass, "left");
+    });
   });
 });
 
 function updateImage(projectClass, direction) {
   const projectImages = imageSources[projectClass];
   let currentIndex = currentIndexes[projectClass];
+
+  if (projectImages.length <= 1) {
+    return;
+  }
 
   if (direction === "left") {
     currentIndex =
